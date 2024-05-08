4f99f61e6d8e744fa223204b200db8c50fe32578
Amgeddd <134081860+Amgeddd@users.noreply.github.com>
2024-05-08 20:14:05 +0300

    Update tooltip.php

--- tooltip.php
+++ tooltip.php
@@ -1,30 +1,31 @@
 <script type="text/javascript">
-    $(document).ready(function(){
-        $('#home').tooltip('show');
-        $('#home').tooltip('hide');
-    });
-</script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#home1').tooltip('show');
-        $('#home1').tooltip('hide');
-    });
-</script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#login').tooltip('show');
-        $('#login').tooltip('hide');
-    });
-</script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#signup').tooltip('show');
-        $('#signup').tooltip('hide');
-    });
-</script>
-<script type="text/javascript">
-    $(document).ready(function(){
-        $('#sort').tooltip('show');
-        $('#sort').tooltip('hide');
-    });
-</script>
+                                        $(document).ready(function(){
+                                            $('#home').tooltip('show')
+                                            $('#home').tooltip('hide')
+                                        });
+                                    </script>
+												<script type="text/javascript">
+                                        $(document).ready(function(){
+                                            $('#home1').tooltip('show')
+                                            $('#home1').tooltip('hide')
+                                        });
+                                    </script>
+									    <script type="text/javascript">
+                                        $(document).ready(function(){
+                                            $('#login').tooltip('show')
+                                            $('#login').tooltip('hide')
+                                        });
+                                    </script>
+										    <script type="text/javascript">
+                                        $(document).ready(function(){
+                                            $('#signup').tooltip('show')
+                                            $('#signup').tooltip('hide')
+                                        });
+                                    </script>
+									
+									    <script type="text/javascript">
+                                        $(document).ready(function(){
+                                            $('#sort').tooltip('show')
+                                            $('#sort').tooltip('hide')
+                                        });
+                                    </script>
